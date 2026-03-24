a93c8a04c0df599a16d2400e0a1a9872208e44a5
teboc <bobllor@yahoo.com>
2026-03-23 23:55:07 -0500

    added API base

--- src/api/api.go
+++ src/api/api.go
@@ -1,4 +1,18 @@
 package api
 
+import (
+	dbcon "github.com/bobllor/cloud-project/src/db_con"
+)
+
+// DBAPI is used for handling API requests.
 type DBAPI struct {
+	FilesDB dbcon.FilesDB
+}
+
+func NewDBAPI(filesDB dbcon.FilesDB) *DBAPI {
+	api := &DBAPI{
+		FilesDB: filesDB,
+	}
+
+	return api
 }
