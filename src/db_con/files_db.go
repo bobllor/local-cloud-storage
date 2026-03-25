e5f95fbfc99ec738348e946dca5a5e9c57bb8700
teboc <bobllor@yahoo.com>
2026-03-24 23:49:30 -0500

    finished AddFile function, updated FilesDB struct and updated code for SQL related code

--- src/db_con/files_db.go
+++ src/db_con/files_db.go
@@ -7,13 +7,19 @@ import (
 	"github.com/bobllor/cloud-project/src/file"
 )
 
+const (
+	FilesTable string = "Files"
+)
+
 type FilesDB struct {
-	database *sql.DB
+	database       *sql.DB
+	fileFieldCount int
 }
 
 func NewFilesDatabase(database *sql.DB) *FilesDB {
 	f := &FilesDB{
-		database: database,
+		database:       database,
+		fileFieldCount: file.FileColumnSize,
 	}
 
 	return f
@@ -27,7 +33,7 @@ func (f *FilesDB) QueryFiles() ([]file.File, error) {
 
 	// TODO: add WHERE filter with user ID when added
 	q, err := f.database.Query(`SELECT 
-		FileName, FileType, FileID, Extension, FilePath, FileSize 
+		FileName, FileType, FileID, FilePath, FileSize 
 		FROM Files`,
 	)
 	if err != nil {
@@ -37,7 +43,7 @@ func (f *FilesDB) QueryFiles() ([]file.File, error) {
 	defer q.Close()
 	for q.Next() {
 		f := file.File{}
-		scanErr := q.Scan(&f.Name, &f.Type, &f.FileID, &f.Ext, &f.Path, &f.Size)
+		scanErr := q.Scan(&f.Name, &f.Type, &f.FileID, &f.Path, &f.Size)
 		if scanErr != nil {
 			return nil, scanErr
 		}
@@ -48,17 +54,37 @@ func (f *FilesDB) QueryFiles() ([]file.File, error) {
 	return files, nil
 }
 
-func (f *FilesDB) AddFile(files ...file.File) error {
+// AddFile adds a File to the Files database.
+// If an error occurs it will return an error.
+//
+// This does not write the files to the disk.
+func (f *FilesDB) AddFile(files []file.File) error {
+	if len(files) == 0 {
+		return fmt.Errorf("no arguments given for AddFile")
+	}
 	tx, err := f.database.Begin()
 	if err != nil {
-		return fmt.Errorf("failed to start transaction: %v", err)
+		return fmt.Errorf("failed to start transaction on %s: %v", FilesTable, err)
 	}
 	defer tx.Rollback()
 
-	query := "INSERT INTO Files VALUES "
-	_, err = tx.Exec(query)
+	flatFiles := file.FlattenFile(files...)
+
+	query := fmt.Sprintf("INSERT INTO %s VALUES", FilesTable)
+	paramStr := QueryParamBuilder(f.fileFieldCount, len(files))
+
+	res, err := tx.Exec(query+" "+paramStr, flatFiles...)
 	if err != nil {
-		return fmt.Errorf("failed to insert into files table: %v", err)
+		return fmt.Errorf("failed to insert into %s: %v", FilesTable, err)
+	}
+
+	rowCount, _ := res.RowsAffected()
+	// TODO: add logging here
+	fmt.Printf("Rows inserted: %v", rowCount)
+
+	err = tx.Commit()
+	if err != nil {
+		return fmt.Errorf("failed to commit transaction for %s: %v", FilesTable, err)
 	}
 
 	return nil
