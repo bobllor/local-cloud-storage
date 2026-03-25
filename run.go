f689f4c06581fdb4cac9ec93cf0e0bfdb6303dcc
teboc <bobllor@yahoo.com>
2026-03-24 23:51:36 -0500

    removed test code in run.go

--- run.go
+++ run.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
-	dbcon "github.com/bobllor/cloud-project/src/db_con"
 	"github.com/joho/godotenv"
 )
 
@@ -26,20 +23,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fileConfig := dbcon.NewConfig(fileUser, os.Getenv("DB_PASSWORD"), "tcp", "127.0.0.1", dbName)
-
-	fileDbCon, err := dbcon.NewDatabase(fileConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	filesDb := dbcon.NewFilesDatabase(fileDbCon)
-
-	files, err := filesDb.QueryFiles()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(files)
 }
