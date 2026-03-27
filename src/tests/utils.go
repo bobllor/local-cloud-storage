dfa0d7e5313db003561299c27312245a9320c5e4
teboc <bobllor@yahoo.com>
2026-03-27 00:33:18 -0500

    added CreateFiles for test utils

---
+++ src/tests/utils.go
@@ -0,0 +1,58 @@
+package tests
+
+import (
+	"math/rand"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+)
+
+// CreateFiles creates random files in the root. It will return
+// a string slice containing the paths of the files created.
+//
+// root is the path where the files will be created in.
+//
+// An error will be returned if any errors occur during
+// the file creation.
+func CreateFiles(root string) ([]string, error) {
+	paths := []string{}
+
+	files := []string{
+		"text1.txt",
+		"text2.txt",
+		"text3.txt",
+		"some.logs.log",
+		"database.db",
+	}
+
+	f1 := "folder1"
+	f2 := "folder2"
+
+	dirPaths := []string{
+		root,
+		filepath.Join(root, f1),
+		filepath.Join(root, f2),
+		filepath.Join(root, f1, f2),
+	}
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for _, file := range files {
+		fullPath := filepath.Join(dirPaths[r.Intn(len(dirPaths)-1)], file)
+		basePath := path.Dir(fullPath)
+
+		err := os.MkdirAll(basePath, 0o744)
+		if err != nil {
+			return nil, err
+		}
+
+		err = os.WriteFile(fullPath, []byte{}, 0o644)
+		if err != nil {
+			return nil, err
+		}
+
+		paths = append(paths, fullPath)
+	}
+
+	return paths, nil
+}
