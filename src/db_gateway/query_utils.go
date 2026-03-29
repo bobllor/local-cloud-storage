f109aeaf7dd8669d0ebfcf84b79302bf012f59c3
teboc <bobllor@yahoo.com>
2026-03-28 22:52:46 -0500

    added query_utils file for query related methods and types

---
+++ src/db_gateway/query_utils.go
@@ -0,0 +1,201 @@
+package dbgateway
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+const (
+	Equal   = "="
+	Gt      = ">"
+	Gte     = ">="
+	Lt      = "<"
+	Lte     = "<="
+	Ne      = "<>"
+	Between = "BETWEEN"
+	Like    = "LIKE"
+	In      = "IN"
+
+	OperatorAnd = "AND"
+	OperatorOr  = "OR"
+)
+
+// NewClauseBuilder creates a new ClauseBuilder.
+func NewClauseBuilder() *ClauseBuilder {
+	return &ClauseBuilder{}
+}
+
+// BuildPlaceholder builds the placeholder string for parameters that are passed into
+// queries.
+//
+// placholderCount is the amount of placeholders that are being used. This is expected
+// to be the number of parameters being used.
+//
+// repeat is how many times to repeat the final placeholder string. This is used for batch operations.
+// If batch operations are not needed then it is expected to be 1. If less than 1 is given,
+// that it will automatically be converted into 1.
+func BuildPlaceholder(placeholderCount int, repeat int) string {
+	questions := []string{}
+	out := []string{}
+
+	if repeat < 1 {
+		repeat = 1
+	}
+
+	for range placeholderCount {
+		questions = append(questions, "?")
+	}
+
+	param := "(" + strings.Join(questions, ",") + ")"
+
+	for range repeat {
+		out = append(out, param)
+	}
+
+	return strings.Join(out, ",")
+}
+
+// DBClauseBuilder is a helper in building dynamic WHERE conditions.
+//
+// When a new clause is registered, the method call will return the same
+// ClauseBuilder. This can be used to chain multiple conditions with AND/OR.
+// However, it must be correct otherwise when building the conditions it will fail.
+type ClauseBuilder struct {
+	// clauses are SQL clauses used in the WHERE condition. The operator
+	// conditions (AND/OR) are also added in here.
+	clauses []string
+
+	// args are the arg parameters for the clause. It is all the args in one
+	// and is not equal to the size of clauses, due to a single clause potentially
+	// having multiple parameters. The args order is the same as when
+	// the clause was added.
+	args []any
+}
+
+// Build combines the registered clauses and returns the clause and
+// a flatten slice of all arguments in order of the clauses.
+//
+// The return query does not include WHERE.
+//
+// The clauses will be validated prior to building, if the clauses
+// are not built correctly then an error will be returned.
+func (c *ClauseBuilder) Build() (string, []any, error) {
+	clauseHolder := []string{}
+
+	err := c.validateEndClause()
+	if err != nil {
+		return "", nil, err
+	}
+
+	err = c.validateClauses()
+	if err != nil {
+		return "", nil, err
+	}
+
+	for _, clause := range c.clauses {
+		clauseHolder = append(clauseHolder, clause)
+	}
+
+	clause := strings.Join(clauseHolder, " ")
+
+	return clause, c.args, nil
+}
+
+// Equal registers a new equal (=) clause to the builder.
+func (c *ClauseBuilder) Equal(column string, arg any) *ClauseBuilder {
+	clause := fmt.Sprintf("%s %s ?", column, Equal)
+
+	c.register(clause, arg)
+
+	return c
+}
+
+// In registers a new IN clause to the builder. This is used for
+// checking values in a list for SQL.
+func (c *ClauseBuilder) In(column string, args ...any) *ClauseBuilder {
+	params := BuildPlaceholder(len(args), 1)
+
+	clause := fmt.Sprintf("%s %s %s", column, In, params)
+
+	c.register(clause, args...)
+
+	return c
+}
+
+// And adds an AND operator to the clauses to prepare the next
+// condition.
+func (c *ClauseBuilder) And() *ClauseBuilder {
+	c.addOperator(OperatorAnd)
+
+	return c
+}
+
+// Or adds an OR operator to the clauses to prepare the next
+// condition.
+func (c *ClauseBuilder) Or() *ClauseBuilder {
+	c.addOperator(OperatorOr)
+
+	return c
+}
+
+// register is a helper function that registers the clause and args to
+// the equivalent fields.
+func (c *ClauseBuilder) register(clause string, args ...any) {
+	c.clauses = append(c.clauses, clause)
+	c.args = append(c.args, args...)
+}
+
+// addOperator adds an operator to clauses to prepare the next
+// clause condition.
+func (c *ClauseBuilder) addOperator(operator string) {
+	c.clauses = append(c.clauses, operator)
+}
+
+// validateClauses checks if a clause is valid. This validates
+// if the clauses and operators are added properly.
+// It does not do input validation on queries.
+//
+// It will return an error if it is not valid.
+func (c *ClauseBuilder) validateClauses() error {
+	if len(c.clauses) == 0 {
+		return fmt.Errorf("clause validation failed: cannot build an empty clause")
+	}
+	operators := []string{OperatorAnd, OperatorOr}
+	mustBeOp := false
+
+	for _, clause := range c.clauses {
+		if mustBeOp && !slices.Contains(operators, clause) {
+			return fmt.Errorf("expected operator (AND/OR) but got clause %s (%v)", clause, c.clauses)
+		} else if !mustBeOp && slices.Contains(operators, clause) {
+			return fmt.Errorf("expected non-operator clause but got %s (%v)", clause, c.clauses)
+		}
+
+		if mustBeOp {
+			mustBeOp = false
+		} else {
+			mustBeOp = true
+		}
+	}
+
+	return nil
+}
+
+// validateEndClause checks if the ending of the clauses is an operator.
+// This is used to drop operators without a follow-up clause.
+// When a normal clause is encountered, then it will stop the search.
+func (c *ClauseBuilder) validateEndClause() error {
+	operators := []string{OperatorAnd, OperatorOr}
+
+	for lp := len(c.clauses) - 1; lp > -1; lp-- {
+		clause := c.clauses[lp]
+
+		if slices.Contains(operators, clause) {
+			return fmt.Errorf("end clauses cannot end in an operator: got %s (%v)", clause, c.clauses)
+		} else {
+			break
+		}
+	}
+
+	return nil
+}
---
+++ src/db_gateway/query_utils_test.go
@@ -0,0 +1,74 @@
+package dbgateway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bobllor/assert"
+	"github.com/bobllor/cloud-project/src/file"
+)
+
+func TestBuildSingleQuery(t *testing.T) {
+	cb := ClauseBuilder{}
+
+	arg1 := "afileid"
+	expectedArgs := 1
+	expectedQuery := fmt.Sprintf("%s = ?", file.FileIDCol)
+
+	cb.Equal(file.FileIDCol, arg1)
+
+	q, args, err := cb.Build()
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(args), expectedArgs)
+	assert.Equal(t, q, expectedQuery)
+}
+
+func TestBuildAndQuery(t *testing.T) {
+	cb := ClauseBuilder{}
+
+	expectedArgs := 2
+
+	arg1 := "fileidhere"
+	arg2 := "filename.txt"
+
+	expectedQuery := fmt.Sprintf("%s = ? AND %s = ?", file.FileIDCol, file.FileNameCol)
+
+	cb.Equal(file.FileIDCol, arg1).And().Equal(file.FileNameCol, arg2)
+
+	q, args, err := cb.Build()
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(args), expectedArgs)
+	assert.Equal(t, args[0], arg1)
+	assert.Equal(t, args[1], arg2)
+
+	assert.Equal(t, q, expectedQuery)
+}
+
+func TestInQuery(t *testing.T) {
+	cb := NewClauseBuilder()
+
+	expectedArgs := 4
+	expectedQuery := fmt.Sprintf("%s = ? AND %s IN (?,?,?)", file.FileOwnerIDCol, file.FileNameCol)
+
+	cb.Equal(file.FileOwnerIDCol, "file-owner").And().In(file.FileNameCol, "test1.txt", "test2.txt", "test3.txt")
+
+	q, args, err := cb.Build()
+	assert.Nil(t, err)
+
+	assert.Equal(t, len(args), expectedArgs)
+	assert.Equal(t, q, expectedQuery)
+}
+
+func TestEndOperatorClauseError(t *testing.T) {
+	cb := NewClauseBuilder()
+
+	cb.Equal(file.FileOwnerIDCol, "fileidhere").
+		And().
+		Equal(file.FileNameCol, "filenamehere.txt").
+		And().And().Or()
+
+	_, _, err := cb.Build()
+	assert.NotNil(t, err)
+}
