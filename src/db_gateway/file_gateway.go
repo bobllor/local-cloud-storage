23a0f79df344bf3593e47ff8483e296e049ef965
teboc <bobllor@yahoo.com>
2026-03-29 01:06:00 -0500

    large overhaul due to: multiple abstractions, new structs/methods

--- src/db_gateway/file_gateway.go
+++ src/db_gateway/file_gateway.go
@@ -6,16 +6,8 @@ import (
 	"time"
 
 	"github.com/bobllor/cloud-project/src/file"
+	"github.com/bobllor/cloud-project/src/utils"
 )
-
-const (
-	FileTable string = "File"
-)
-
-type FileGateway struct {
-	database       *sql.DB
-	fileFieldCount int
-}
 
 func NewFileGateway(database *sql.DB) *FileGateway {
 	f := &FileGateway{
@@ -26,24 +18,158 @@ func NewFileGateway(database *sql.DB) *FileGateway {
 	return f
 }
 
-// QueryFile queries the File table and returns a File slice.
+type FileGateway struct {
+	database       *sql.DB
+	fileFieldCount int
+}
+
+// GetAllFiles returns a File slice of all File rows belonging to the file owner.
+//
 // If an error occurs then it will return an error, and abort
 // the scanning process if it is occurring.
-func (f *FileGateway) QueryFile(fileOwnerID string) ([]file.File, error) {
-	files := []file.File{}
+func (f *FileGateway) GetAllFiles(fileOwnerID string) ([]file.File, error) {
+	cb := NewClauseBuilder()
+	baseQuery := fmt.Sprintf("SELECT * FROM %s", file.FileTableName)
+	cb.Equal(file.FileOwnerIDCol, fileOwnerID)
 
-	query := fmt.Sprintf("SELECT * FROM %s WHERE %s = ?", FileTable, file.FileOwnerIDCol)
-	q, err := f.database.Query(query, fileOwnerID)
+	con, args, err := cb.Build()
 	if err != nil {
-		return nil, fmt.Errorf("failed to query %s: %v", FileTable, err)
+		return nil, fmt.Errorf("failed to build conditions: %v", err)
 	}
 
-	defer q.Close()
-	for q.Next() {
+	query := baseQuery + " " + con
+
+	rows, err := f.database.Query(query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query %s: %v", file.FileTableName, err)
+	}
+
+	files, err := f.getFiles(rows)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan rows: %v", err)
+	}
+
+	return files, nil
+}
+
+// GetFiles returns File rows based on the clause and conditions.
+//
+// filters is a slice of FileFilter that are used to build the conditions
+// after the file owner condition. All fields of FileFilter must have an entry.
+func (f *FileGateway) GetFiles(fileOwnerID string, filters []FileFilter) ([]file.File, error) {
+	cb := NewClauseBuilder()
+
+	cb.Equal(file.FileOwnerIDCol, fileOwnerID)
+	baseQuery := fmt.Sprintf("SELECT * FROM %s", file.FileTableName)
+
+	err := QueryFromFilters(cb, filters)
+	if err != nil {
+		return nil, err
+	}
+
+	q, args, err := cb.Build()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %v", err)
+	}
+
+	query := baseQuery + " " + q
+
+	// TODO: log here
+	fmt.Println(query)
+
+	rows, err := f.database.Query(query, args...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to query: %v", err)
+	}
+
+	files, err := f.getFiles(rows)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan rows: %v", err)
+	}
+
+	return files, nil
+}
+
+// AddFile adds slice of File structs to the File database.
+// If an error occurs it will return an error.
+//
+// This does not write the files to the disk.
+func (f *FileGateway) AddFile(files []file.File) error {
+	if len(files) == 0 {
+		return fmt.Errorf("no arguments given for AddFile")
+	}
+
+	flatFiles := file.FlattenFile(files...)
+
+	query := fmt.Sprintf("INSERT INTO %s VALUES", file.FileTableName)
+	paramStr := BuildPlaceholder(f.fileFieldCount, len(files))
+
+	res, err := execQuery(f.database, query+" "+paramStr, flatFiles...)
+	if err != nil {
+		return fmt.Errorf("failed to insert into %s: %v", file.FileTableName, err)
+	}
+
+	rowCount, err := res.RowsAffected()
+	// TODO: add logging here
+	fmt.Printf("Rows inserted: %v", rowCount)
+
+	return nil
+}
+
+// DeleteFile sets a slice of file IDs to be marked for deletion.
+// This does not delete the files immediately.
+func (f *FileGateway) DeleteFiles(fileOwnerID string, fileIDs []string) error {
+	cb := NewClauseBuilder()
+
+	if len(fileIDs) == 0 {
+		return fmt.Errorf("failed to delete files, got empty file IDs")
+	}
+
+	baseQuery := fmt.Sprintf(
+		"UPDATE %s SET %s = '%v'",
+		file.FileTableName,
+		file.DeletedDateCol,
+		time.Now().Format(time.DateTime),
+	)
+
+	convFileIDs := utils.ConvertToAny(fileIDs)
+
+	cb.Equal(file.FileOwnerIDCol, fileOwnerID).And().In(file.FileIDCol, convFileIDs...)
+
+	qCondition, args, err := cb.Build()
+	if err != nil {
+		return fmt.Errorf("failed to build condition query: %v", err)
+	}
+
+	query := baseQuery + " " + qCondition
+
+	// TODO: add logging
+	fmt.Println(query)
+
+	res, err := execQuery(f.database, query, args...)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(res.RowsAffected())
+
+	return nil
+}
+
+// getFiles is a helper function used to scan and return
+// a slice of Files.
+//
+// sql.Rows will automatically be closed at the end of function.
+func (f *FileGateway) getFiles(rows *sql.Rows) ([]file.File, error) {
+	files := []file.File{}
+
+	for rows.Next() {
 		f := file.File{}
+		// datetime is returned as a []uint38, uint38 -> string -> date
 		modifiedTimeSl := make([]uint8, 0)
 		deletedTimeSl := make([]uint8, 0)
-		scanErr := q.Scan(
+
+		scanErr := rows.Scan(
 			&f.OwnerID,
 			&f.Name,
 			&f.Type,
@@ -84,37 +210,29 @@ func (f *FileGateway) QueryFile(fileOwnerID string) ([]file.File, error) {
 	return files, nil
 }
 
-// AddFile adds slice of [File] structs to the File database.
-// If an error occurs it will return an error.
-//
-// This does not write the files to the disk.
-func (f *FileGateway) AddFile(files []file.File) error {
-	if len(files) == 0 {
-		return fmt.Errorf("no arguments given for AddFile")
-	}
-	tx, err := f.database.Begin()
-	if err != nil {
-		return fmt.Errorf("failed to start transaction on %s: %v", FileTable, err)
-	}
-	defer tx.Rollback()
+// TODO: make everything below this better. for now its temporary for a baseline!
 
-	flatFiles := file.FlattenFile(files...)
+type FileFilter struct {
+	Column   string
+	Args     []any
+	Type     string
+	Operator string
+}
 
-	query := fmt.Sprintf("INSERT INTO %s VALUES", FileTable)
-	paramStr := QueryParamBuilder(f.fileFieldCount, len(files))
+// QueryFromFilters registers clauses from a slice of FileFilters.
+func QueryFromFilters(cb *ClauseBuilder, fileFilters []FileFilter) error {
+	for _, filter := range fileFilters {
+		if filter.Operator == "AND" {
+			cb.And()
+		} else {
+			cb.Or()
+		}
 
-	res, err := tx.Exec(query+" "+paramStr, flatFiles...)
-	if err != nil {
-		return fmt.Errorf("failed to insert into %s: %v", FileTable, err)
-	}
-
-	rowCount, _ := res.RowsAffected()
-	// TODO: add logging here
-	fmt.Printf("Rows inserted: %v", rowCount)
-
-	err = tx.Commit()
-	if err != nil {
-		return fmt.Errorf("failed to commit transaction for %s: %v", FileTable, err)
+		if filter.Type == "EQUAL" {
+			cb.Equal(filter.Column, filter.Args[0])
+		} else {
+			cb.In(filter.Column, filter.Args...)
+		}
 	}
 
 	return nil
