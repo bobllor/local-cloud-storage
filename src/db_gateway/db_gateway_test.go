445a34a3387cce8c168ad96658a497834f468dfb
teboc <bobllor@yahoo.com>
2026-04-01 10:54:55 -0500

    added test case for ClauseData

--- src/db_gateway/db_gateway_test.go
+++ src/db_gateway/db_gateway_test.go
@@ -1,1 +1,23 @@
 package dbgateway
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bobllor/assert"
+	"github.com/bobllor/cloud-project/src/file"
+)
+
+func TestClauseDataBuildSetQuery(t *testing.T) {
+	cd := ClauseData{
+		Columns: []string{file.FileIDCol, file.FileNameCol},
+		Args:    []any{testFileID, "a file text.txt"},
+	}
+
+	expectedQuery := fmt.Sprintf("SET %s = ?,%s = ?", file.FileIDCol, file.FileNameCol)
+
+	setQ, err := cd.BuildSetQuery()
+	assert.Nil(t, err)
+
+	assert.Equal(t, setQ, expectedQuery)
+}
