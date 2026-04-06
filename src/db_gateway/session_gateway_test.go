76435c2058a282aabe830ce6ee26ede138a68a20
teboc <bobllor@yahoo.com>
2026-04-06 13:05:31 -0500

    added test cases to handle updating and new insertion of data for upsert

--- src/db_gateway/session_gateway_test.go
+++ src/db_gateway/session_gateway_test.go
@@ -4,10 +4,12 @@ import (
 	"io"
 	"log"
 	"testing"
+	"time"
 
 	"github.com/bobllor/assert"
 	"github.com/bobllor/cloud-project/src/config"
 	"github.com/bobllor/cloud-project/src/session"
+	"github.com/bobllor/cloud-project/src/user"
 	"github.com/bobllor/gologger"
 )
 
@@ -26,14 +28,38 @@ func TestGetSession(t *testing.T) {
 	assert.Equal(t, s.SessionID, testSessionID)
 }
 
-func TestAddSession(t *testing.T) {
+func TestUpsertSessionNew(t *testing.T) {
+	ug, err := newTestUserGateway()
+	assert.Nil(t, err)
+	sg, err := newTestSessionGateway()
+	assert.Nil(t, err)
+
+	usr, err := ug.AddUser("username.here", "password")
+	assert.Nil(t, err)
+
+	addS, err := sg.UpsertSession(usr.AccountID)
+	assert.Nil(t, err)
+
+	sesh, err := sg.GetSession(addS.AccountID)
+	assert.Nil(t, err)
+
+	_, err = DropRows(ug.database, user.TableName, user.ColumnAccountID, usr.AccountID)
+	assert.Nil(t, err)
+
+	assert.Equal(t, sesh.AccountID, addS.AccountID)
+	assert.Equal(t, sesh.SessionID, addS.SessionID)
+	assert.Equal(t, sesh.CreatedOn.Truncate(time.Minute), addS.CreatedOn.Truncate(time.Minute))
+	assert.Equal(t, sesh.ExpireOn.Truncate(time.Minute), addS.ExpireOn.Truncate(time.Minute))
+}
+
+func TestUpsertSessionReplace(t *testing.T) {
 	sg, err := newTestSessionGateway()
 	assert.Nil(t, err)
 
 	baseS, err := sg.GetSession(testUserAccountID)
 	assert.Nil(t, err)
 
-	err = sg.AddSession(testUserAccountID)
+	sesh, err := sg.UpsertSession(testUserAccountID)
 	assert.Nil(t, err)
 
 	newS, err := sg.GetSession(testUserAccountID)
@@ -55,6 +81,10 @@ func TestAddSession(t *testing.T) {
 	assert.NotEqual(t, baseS.SessionID, newS.SessionID)
 	assert.Equal(t, baseS.CreatedOn.Compare(newS.CreatedOn), -1)
 	assert.Equal(t, baseS.ExpireOn.Compare(newS.ExpireOn), -1)
+
+	assert.Equal(t, newS.SessionID, sesh.SessionID)
+	assert.True(t, newS.CreatedOn.Truncate(time.Minute).Equal(sesh.CreatedOn.Truncate(time.Minute)))
+	assert.True(t, newS.ExpireOn.Truncate(time.Minute).Equal(sesh.ExpireOn.Truncate(time.Minute)))
 }
 
 func newTestSessionGateway() (*SessionGateway, error) {
