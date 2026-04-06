77968b6afbb3737c1fd5c6fe64e0b0702baa064b
teboc <bobllor@yahoo.com>
2026-04-06 16:57:50 -0500

    added CheckCredentials and changed GetUser by using ID and AccountID

--- src/db_gateway/user_gateway.go
+++ src/db_gateway/user_gateway.go
@@ -69,8 +69,66 @@ func (ug *UserGateway) AddUser(username string, password string) (*user.UserAcco
 	return acc, err
 }
 
-// GetUser retrieves the full row of a single user.
-func (ug *UserGateway) GetUser(accountID string) (*user.UserAccount, error) {
+// ConfirmUser confirms if the credentials are correct for the user. The username and
+// password is compared and will return a boolean or an error if one occurs.
+//
+// An error will be returned if the user does not exist in the database.
+func (ug *UserGateway) CheckCredentials(username string, password string) (bool, error) {
+	user, err := ug.GetUserByUsername(username)
+	if err != nil {
+		return false, err
+	}
+
+	storedHash, err := hasher.ParsePHC(user.PasswordHash)
+	if err != nil {
+		return false, err
+	}
+
+	validCredentials, err := hasher.Compare(password, storedHash)
+	if err != nil {
+		return false, err
+	}
+
+	return validCredentials, nil
+}
+
+// GetUserByUsername gets the user row based on the username.
+// If the username does not exist in the database, an error will be returned,
+// otherwise standard errors will occur.
+func (ug *UserGateway) GetUserByUsername(username string) (*user.UserAccount, error) {
+	cb := NewClauseBuilder()
+	cb.Equal(user.ColumnUsername, username)
+
+	baseQ := fmt.Sprintf("SELECT * FROM %s", user.TableName)
+
+	cbQ, args, err := cb.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	query := baseQ + " " + cbQ
+
+	rows, err := ug.database.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	users := []user.UserAccount{}
+
+	err = SelectRows(rows, &users)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(users) == 0 {
+		return nil, fmt.Errorf("username %s does not exist in database", username)
+	}
+
+	return &users[0], nil
+}
+
+// GetUserByID retrieves the user row based on the account ID.
+func (ug *UserGateway) GetUserByID(accountID string) (*user.UserAccount, error) {
 	cb := NewClauseBuilder()
 	cb.Equal(user.ColumnAccountID, accountID)
 
