90a975246eb4ae6937a35903841c5856da92d89f
teboc <bobllor@yahoo.com>
2026-04-08 01:12:33 -0500

    updated api.go for handler creation

--- src/api/api.go
+++ src/api/api.go
@@ -1,12 +1,14 @@
 package api
 
 import (
+	"net/http"
+
 	dbcon "github.com/bobllor/cloud-project/src/db_gateway"
 )
 
-type Api struct {
-	User *UserHandler
-}
+const (
+	ContentJson = "application/json"
+)
 
 // NewApi creates a new Api struct.
 func NewApi(gw *dbcon.Gateway) *Api {
@@ -15,4 +17,33 @@ func NewApi(gw *dbcon.Gateway) *Api {
 	}
 
 	return api
+}
+
+type Api struct {
+	User     *UserHandler
+	Handlers HandlerMap
+}
+
+// GetHandlers
+func (a *Api) GetHandlers() HandlerMap {
+	return a.Handlers
+}
+
+// addUserHandlers is used to add the HandlerFunc with a route, used for the HandlerMap.
+// This function will panic if a duplicate handler is added.
+func (a *Api) addUserHandlers() {
+	u := a.User
+
+	a.addHandler(UserRegisterRoute, u.Post.RegisterUser)
+}
+
+// addHandler adds a handler to a.HandlerMap. If a duplicate route is found, then this method
+// will panic.
+func (a *Api) addHandler(route string, handleFunc func(w http.ResponseWriter, r *http.Request)) {
+	_, ok := a.Handlers[route]
+	if !ok {
+		a.Handlers[route] = handleFunc
+	} else {
+		panic("duplicate route found")
+	}
 }
