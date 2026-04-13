9d9539aa1a5908387a6b86efca0df52172a8fca3
teboc <bobllor@yahoo.com>
2026-04-13 00:37:05 -0500

    added httpError helper

--- src/api/handler.go
+++ src/api/handler.go
@@ -5,3 +5,13 @@ import "net/http"
 type HandlerMap map[string]func(http.ResponseWriter, *http.Request)
 
 type Handler interface{}
+
+// httpError is a helper function used to write an error if one occurred.
+// This does not check if err != nil. If err == nil then this will do nothing.
+func httpError(w http.ResponseWriter, err error, statusCode int) {
+	if err == nil {
+		return
+	}
+
+	http.Error(w, err.Error(), statusCode)
+}
