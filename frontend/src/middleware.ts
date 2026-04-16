72c3232ab16975da4fd2b38737e4b294262db174
teboc <bobllor@yahoo.com>
2026-04-16 01:13:39 -0500

    added placeholder middleware function

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,9 @@
+/**
+ * A request will be sent to the backend for authentication using the cookie
+ * in the request header.
+ * 
+ * Authentication failures will redirect to the login page.
+ */
+export async function authMiddleware(){
+
+}
