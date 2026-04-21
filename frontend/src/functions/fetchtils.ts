311025d567c42024ab961a9200e8541ce52f6aba
teboc <bobllor@yahoo.com>
2026-04-21 00:10:54 -0500

    added fetch utils

---
+++ frontend/src/functions/fetchtils.ts
@@ -0,0 +1,16 @@
+/**
+ * Sends a GET request to the given path and returns the response.
+ * If args are used, it will send the data with args.
+ * 
+ * @param path
+ * @returns Response promise
+ */
+export async function getRequest(path: string, body?: {}): Promise<Response>{
+    const res = await fetch(path, {
+        method: "GET",
+        body: !body ? undefined : JSON.stringify(body),
+        credentials: "include",
+    })
+
+    return res;
+}
