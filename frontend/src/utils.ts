768bf283cd879f45186aa67126892ad763bf35cf
teboc <bobllor@yahoo.com>
2026-04-24 02:48:34 -0500

    updated createUrl to optionally include forward slash

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,13 +1,17 @@
 import { SERVER_BASE_URL } from "./env";
 
 /**
- * Creates the URL from the given base URL in the .env file. This requires the .env file
+ * Creates the URL from the base URL in the .env file. This requires the .env file
  * located in the root of the React files.
  *  
- * @param path The string path of the url, this must start with a forward slash.
+ * @param path The string path of the url, this is the non-base path
  * @returns The combined URL
  */
 export function createUrl(path: string): string{
+    if(path.length > 0 && path[0] !== "/"){
+        path = "/" + path;
+    }
+
     var url: string = SERVER_BASE_URL + path;
 
     return url;
