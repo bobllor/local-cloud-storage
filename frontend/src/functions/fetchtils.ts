5bb80165bd28df1991723efcb437a9921cf6b8b6
teboc <bobllor@yahoo.com>
2026-04-24 02:39:53 -0500

    updated getRequest to request for generic requests

--- frontend/src/functions/fetchtils.ts
+++ frontend/src/functions/fetchtils.ts
@@ -8,22 +8,24 @@ import { createUrl } from "../utils";
  * @returns The session validation status
  */
 export async function validateSession(): Promise<boolean>{
-    const res = await getRequest(createUrl("/api/session"));
+    const res = await request(createUrl("/api/session"));
     const output: ResponseApi<boolean> = await res.json();
 
     return output.output;
 }
 
 /**
- * Sends a GET request to the given path and returns the response.
+ * Sends a request to the given path and returns the response.
  * If args are used, it will send the data with args.
  * 
- * @param path
+ * @param path The path of the request URL
+ * @param method The method used on the request
+ * @param body A BodyInit object used to pass data to the backend
  * @returns Response promise
  */
-export async function getRequest(path: string, body?: {}): Promise<Response>{
+export async function request(path: string, method: string = "GET", body?: BodyInit): Promise<Response>{
     const res = await fetch(path, {
-        method: "GET",
+        method: method,
         body: !body ? undefined : JSON.stringify(body),
         credentials: "include",
     })
