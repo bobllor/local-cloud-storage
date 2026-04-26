d8c0a361f6eff2e8e688b5345c0c49323443d22f
teboc <bobllor@yahoo.com>
2026-04-25 20:36:28 -0500

    added new code for errors

--- src/api/errors.go
+++ src/api/errors.go
@@ -12,4 +12,16 @@ var (
 	// ReasonUserAlreadyExists is used for when the SQL database rejects the user
 	// due to a duplicate entry.
 	ReasonUserAlreadyExists ReasonCode = "USER_ALREADY_EXISTS"
+
+	// ReasonBadUsername is used to indicate the given username failed validation.
+	ReasonBadUsername ReasonCode = "BAD_USERNAME"
+
+	// ReasonUnauthorized is used for unauthenticated requests.
+	ReasonUnauthorized ReasonCode = "UNAUTHORIZED"
+)
+
+var (
+	ErrorInternalErrorMsg = "An unexpected internal error has occurred"
+	ErrorUnauthorizedMsg  = "Unauthorized access"
+	ErrorBadDataMsg       = "Bad request data"
 )
