bc9715fcbff8b59387f2fa95b2233603c899ae51
teboc <bobllor@yahoo.com>
2026-04-25 20:21:50 -0500

    added docstring for response errors

--- src/api/errors.go
+++ src/api/errors.go
@@ -1,7 +1,15 @@
 package api
 
 var (
-	ReasonInternalError     ReasonCode = "INTERNAL_ERROR"
-	ReasonBadRequestData    ReasonCode = "BAD_DATA"
+	// ReasonInternalError is used for an internal server error. This should only
+	// be used for CRITICAL and FATAL level errors.
+	ReasonInternalError ReasonCode = "INTERNAL_ERROR"
+
+	// ReasonBadRequestData is used if the response body is invalid or fails to meet
+	// requirements of a method when its consumed.
+	ReasonBadRequestData ReasonCode = "BAD_DATA"
+
+	// ReasonUserAlreadyExists is used for when the SQL database rejects the user
+	// due to a duplicate entry.
 	ReasonUserAlreadyExists ReasonCode = "USER_ALREADY_EXISTS"
 )
