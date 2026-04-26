3c1c38d5d75f87dc91f8867618f80e23fba8fcf2
teboc <bobllor@yahoo.com>
2026-04-25 20:14:47 -0500

    added error values for responses

---
+++ src/api/errors.go
@@ -0,0 +1,7 @@
+package api
+
+var (
+	ReasonInternalError     ReasonCode = "INTERNAL_ERROR"
+	ReasonBadRequestData    ReasonCode = "BAD_DATA"
+	ReasonUserAlreadyExists ReasonCode = "USER_ALREADY_EXISTS"
+)
