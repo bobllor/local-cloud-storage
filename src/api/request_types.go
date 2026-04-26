7c57d6cfe9c0e9bfd5e5e90c168fdaff197af766
teboc <bobllor@yahoo.com>
2026-04-25 20:16:09 -0500

    added ConfirmPassword to RequestUserInfo

--- src/api/request_types.go
+++ src/api/request_types.go
@@ -2,6 +2,7 @@ package api
 
 // RequestUserInfo is the request body containing the login information of the user.
 type RequestUserInfo struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username        string `json:"username"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirmPassword"`
 }
