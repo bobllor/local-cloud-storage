96da6008d95739e8b1587bd74b4dd1adecfeb6cd
teboc <bobllor@yahoo.com>
2026-04-27 10:19:56 -0500

    added new temp usage

--- frontend/src/components/auth-components/storage-components/StorageHome.tsx
+++ frontend/src/components/auth-components/storage-components/StorageHome.tsx
@@ -1,13 +1,14 @@
 import { type JSX } from "react";
 import { NavLink, useLoaderData } from "react-router";
+import { type User } from "../../../middleware";
 
 export default function StorageHome(): JSX.Element{
-    const loaderData = useLoaderData();
+    const loaderData = useLoaderData<User>();
 
     return (
         <>
-            <div>
-                TEMPORARY: You are logged in.
+            <div className="flex flex-col justify-center items-center">
+                TEMPORARY: Hello {loaderData.username}. You are logged in.
                 <NavLink to={"/"}>
                     Home
                 </NavLink>
