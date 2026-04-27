ca64cea1b49eb00597b76dbe43fd09cff225bbc8
teboc <bobllor@yahoo.com>
2026-04-27 10:18:46 -0500

    added ReasonCode to frontend

--- frontend/src/response-types.ts
+++ frontend/src/response-types.ts
@@ -7,12 +7,17 @@
 export type ResponseStatus = "success" | "error";
 
 /**
- * Represents a successful API response.
+ * Represents a successful API response. The output can be of type T depending on
+ * how the endpoint is configured. Output is only optional if an error occurs.
  */
 export type ResponseApi<T> = {
-    status: ResponseStatus
+    status: "success"
     output: T
-    error?: ResponseError
+    error: never
+} | {
+    status: "error"
+    output: never
+    error: ResponseError
 }
 
 /**
@@ -21,5 +26,25 @@ export type ResponseApi<T> = {
  */
 export type ResponseError = {
     code: number
+    reason: ReasonCode
     message: string
 }
+
+/**
+ * Codes for ReasonCode values used to determine what error has occurred
+ * with the response return.
+ */
+export const REASON_CODES = {
+    internalError: "INTERNAL_ERROR",
+	badRequestData: "BAD_DATA",
+	userAlreadyExists: "USER_ALREADY_EXISTS",
+	badUsername: "BAD_USERNAME",
+	badPassword:"BAD_PASSWORD",
+	unauthorized: "UNAUTHORIZED",
+}
+
+/**
+ * ReasonCode indicates what error type had occurred during the endpoint
+ * call.
+ */
+export type ReasonCode = keyof typeof REASON_CODES
