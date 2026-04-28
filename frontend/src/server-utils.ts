fc623325ef2c26ca77385953df8f1898b2b9b2bf
teboc <bobllor@yahoo.com>
2026-04-28 10:22:29 -0500

    changed utils to server-utils

--- frontend/src/server-utils.ts
+++ frontend/src/server-utils.ts
@@ -3,6 +3,7 @@ import { SERVER_BASE_URL } from "./env";
 /**
  * Creates the URL from the base URL in the .env file. This requires the .env file
  * located in the root of the React files.
+ * If the given path does not include the forward slash, it will be given one.
  *  
  * @param path The string path of the url, this is the non-base path
  * @returns The combined URL
