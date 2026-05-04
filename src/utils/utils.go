8111e683ef30716480854e3886b8c03712ffeede
bobllor <bobllor@yahoo.com>
2026-05-03 19:23:36 -0500

    added file retrieval with root

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,6 +1,11 @@
 package utils
 
-import "time"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
 
 // ConvertToAny converts a slice to an []any. This is only used for
 // for query arguments.
@@ -18,4 +23,24 @@ func ConvertToAny[S ~[]T, T comparable](v S) []any {
 // as a string.
 func FormatTime(date time.Time) string {
 	return date.Format(time.DateTime)
+}
+
+// GetFiles retrieves all files in a given path.
+// It will return a map with the key being the lowercased file name
+// and the value being the absolute path of the file.
+//
+// This does not recursively search through directories.
+func GetFiles(root string) (map[string]string, error) {
+	files := make(map[string]string)
+
+	entries, err := os.ReadDir(root)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ent := range entries {
+		files[strings.ToLower(ent.Name())] = filepath.Join(root, ent.Name())
+	}
+
+	return files, nil
 }
